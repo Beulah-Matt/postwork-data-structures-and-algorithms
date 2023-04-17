c8aeff9e3ee7bfaf04bb424d10423ec6403ea730
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-17 20:33:24 +0300

    Reversing a string algo

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,5 +1,18 @@
 def reverse_string(str)
   # type your code in here
+  # given a string, the characters should flow from last to first
+  # check for the index of the last leter of the word.
+  # Will have to split the string into an array to access the index . str.split("")
+  # move it to the front. 
+  # then place the next letter after it
+  # Move the rest of the characters to the next index
+  # Do this for all the characters
+  # Put them back together. 
+  split_string = str.split("")
+  # space = ''
+  reversed = []
+  str.size.times {reversed << split_string.pop} #then place the next letter after it
+  reversed.join
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -12,6 +25,8 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", reverse_string('catbaby')
 
   # Don't forget to add your own!
+  puts "Expecting: 'acirema si sith'"
+  puts "=>", reverse_string('this is america')
 end
 
 # Please add your pseudocode to this file
